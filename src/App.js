156757c7ca5b1832f89ebc8935fd0018ac2be19b
makrenka <smokrenko79@gmail.com>
2023-02-21 16:30:51 +0300

    add methods

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+export class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      counter: this.props.counter,
+    }
   }
+
+  increaseCounter = () => {
+    if (this.state.counter < 10) {
+      this.setState((state) => ({
+        counter: state.counter + 1,
+      }));
+    };
+  };
+
+  decreaseCounter = () => {
+    if (this.state.counter > -10) {
+      this.setState((state) => ({
+        counter: state.counter - 1,
+      }));
+    };
+  };
+
+  randomCounter = () => {
+    this.setState({
+      counter: +(Math.random(this.state.counter) * (10 - -10) + -10).toFixed(0),
+    });
+  };
+
+  resetCounter = () => {
+    this.setState({
+      counter: this.props.counter,
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <div className="counter">10</div>
+        <div className="counter">{this.state.counter}</div>
         <div className="controls">
-          <button>INC</button>
-          <button>DEC</button>
-          <button>RND</button>
-          <button>RESET</button>
+          <button onClick={this.increaseCounter}>INC</button>
+          <button onClick={this.decreaseCounter}>DEC</button>
+          <button onClick={this.randomCounter}>RND</button>
+          <button onClick={this.resetCounter}>RESET</button>
         </div>
       </div>
     );
   }
 }
-export default App;
