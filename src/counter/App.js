ee0f4ab6697ccdd3ac28848c43f2ff1601489b49
makrenka <smokrenko79@gmail.com>
2023-03-31 18:08:57 +0300

    conversion App to functional component

--- src/counter/App.js
+++ src/counter/App.js
@@ -1,54 +1,91 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import './App.css';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
+export const App = () => {
 
-    this.state = {
-      counter: this.props.counter,
-    }
-  }
+  const [counter, setCounter] = useState(0);
 
-  increaseCounter = () => {
-    if (this.state.counter < 10) {
-      this.setState((state) => ({
-        counter: state.counter + 1,
-      }));
+  const increaseCounter = () => {
+    if (counter < 10) {
+      setCounter((counter) => counter + 1);
     };
   };
 
-  decreaseCounter = () => {
-    if (this.state.counter > -10) {
-      this.setState((state) => ({
-        counter: state.counter - 1,
-      }));
+  const decreaseCounter = () => {
+    if (counter > -10) {
+      setCounter((counter) => counter - 1);
     };
   };
 
-  randomCounter = () => {
-    this.setState({
-      counter: +(Math.random(this.state.counter) * (10 - -10) + -10).toFixed(0),
-    });
+  const randomCounter = () => {
+    setCounter(+(Math.random(counter) * (10 - -10) + -10).toFixed(0));
   };
 
-  resetCounter = () => {
-    this.setState({
-      counter: this.props.counter,
-    });
+  const resetCounter = () => {
+    setCounter(0);
   };
 
-  render() {
-    return (
-      <div className="app">
-        <div className="counter">{this.state.counter}</div>
-        <div className="controls">
-          <button onClick={this.increaseCounter}>INC</button>
-          <button onClick={this.decreaseCounter}>DEC</button>
-          <button onClick={this.randomCounter}>RND</button>
-          <button onClick={this.resetCounter}>RESET</button>
-        </div>
+  return (
+    <div className="app">
+      <div className="counter">{counter}</div>
+      <div className="controls">
+        <button onClick={increaseCounter}>INC</button>
+        <button onClick={decreaseCounter}>DEC</button>
+        <button onClick={randomCounter}>RND</button>
+        <button onClick={resetCounter}>RESET</button>
       </div>
-    );
-  }
-}
+    </div>
+  )
+};
+
+// export class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       counter: 0,
+//     }
+//   }
+
+//   increaseCounter = () => {
+//     if (this.state.counter < 10) {
+//       this.setState((state) => ({
+//         counter: state.counter + 1,
+//       }));
+//     };
+//   };
+
+//   decreaseCounter = () => {
+//     if (this.state.counter > -10) {
+//       this.setState((state) => ({
+//         counter: state.counter - 1,
+//       }));
+//     };
+//   };
+
+//   randomCounter = () => {
+//     this.setState({
+//       counter: +(Math.random(this.state.counter) * (10 - -10) + -10).toFixed(0),
+//     });
+//   };
+
+//   resetCounter = () => {
+//     this.setState({
+//       counter: 0,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <div className="counter">{this.state.counter}</div>
+//         <div className="controls">
+//           <button onClick={this.increaseCounter}>INC</button>
+//           <button onClick={this.decreaseCounter}>DEC</button>
+//           <button onClick={this.randomCounter}>RND</button>
+//           <button onClick={this.resetCounter}>RESET</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import { App } from './counter/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App counter={0} />
+    <App />
   </React.StrictMode>
 );
