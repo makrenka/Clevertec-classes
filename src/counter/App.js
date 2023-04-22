a8c3559ef11118f288e88a83dfb5cffb3ca87a40
makrenka <smokrenko79@gmail.com>
2023-04-22 10:57:47 +0300

    add reducer

--- src/counter/App.js
+++ src/counter/App.js
@@ -1,48 +1,98 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Spinner } from './Spinner';
 import { ErrorMessage } from './ErrorMessage';
 import './App.css';
 import { useService } from './service';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      if (state.count >= 20) { return { count: 20 } }
+      else {
+        return {
+          count: state.count + 1
+        }
+      };
+    case 'decrement':
+      if (state.count <= 1) { return { count: 1 } }
+      else {
+        return {
+          count: state.count - 1
+        }
+      };
+    case 'random':
+      return {
+        count: +(Math.random(state.count) * (20 - 1) + 1).toFixed(0)
+      };
+    case 'reset':
+      return {
+        count: 1
+      };
+    default:
+      throw new Error();
+  };
+};
+
 export const App = () => {
 
-  const [counter, setCounter] = useState(1);
+  // const [counter, setCounter] = useState(1);
   const [image, setImage] = useState(null);
+  const [autoplay, setAutoplay] = useState(false);
 
   const { loading, error, clearError, getImage } = useService();
+
+  const [state, dispatch] = useReducer(reducer, { count: 1 })
 
   const onImageLoaded = (image) => {
     setImage(image);
   };
 
-  const loadImage = (counter) => {
+  const loadImage = () => {
     clearError();
-    getImage(counter).then(onImageLoaded);
+    getImage(state.count).then(onImageLoaded);
   };
 
   useEffect(() => {
-    loadImage(counter);
-  }, [counter]);
+    loadImage();
+  }, [state.count]);
 
-  const increaseCounter = () => {
-    if (counter < 20) {
-      setCounter((counter) => counter + 1);
+  // const increaseCounter = () => {
+  //   setAutoplay(false);
+  //   if (counter < 20) {
+  //     setCounter((counter) => counter + 1);
+  //   };
+  // };
+
+  // const decreaseCounter = () => {
+  //   setAutoplay(false);
+  //   if (counter > 1) {
+  //     setCounter((counter) => counter - 1);
+  //   };
+  // };
+
+  // const randomCounter = () => {
+  //   setAutoplay(false);
+  //   setCounter(+(Math.random(counter) * (20 - 1) + 1).toFixed(0));
+  // };
+
+  // const resetCounter = () => {
+  //   setAutoplay(false);
+  //   setCounter(1);
+  // };
+
+  const toggleAutoplay = () => {
+    setAutoplay((autoplay) => !autoplay);
+  }
+
+  useEffect(() => {
+    if (!autoplay) return;
+
+    const interval = setInterval((() => dispatch({ type: 'increment' })), 3000);
+
+    return () => {
+      clearInterval(interval);
     };
-  };
-
-  const decreaseCounter = () => {
-    if (counter > 1) {
-      setCounter((counter) => counter - 1);
-    };
-  };
-
-  const randomCounter = () => {
-    setCounter(+(Math.random(counter) * (20 - 1) + 1).toFixed(0));
-  };
-
-  const resetCounter = () => {
-    setCounter(1);
-  };
+  }, [autoplay]);
 
   if (loading) return <Spinner />;
   if (error) return <ErrorMessage />;
@@ -50,12 +100,13 @@ export const App = () => {
     return (
       <div className="app">
         <img src={image.thumbnailUrl} alt='placeholder-img' className='slide-img' />
-        <div className="counter">{counter}</div>
+        <div className="counter">{state.count}</div>
         <div className="controls">
-          <button onClick={decreaseCounter}>DEC</button>
-          <button onClick={increaseCounter}>INC</button>
-          <button onClick={randomCounter}>RND</button>
-          <button onClick={resetCounter}>RESET</button>
+          <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+          <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+          <button onClick={() => dispatch({ type: 'random' })}>RND</button>
+          <button onClick={() => dispatch({ type: 'reset' })}>RESET</button>
+          <button onClick={toggleAutoplay}>AUTOPLAY</button>
         </div>
       </div>
     );
